a2b771a607c3fb080661ace2d39553518d98e0d2
Matheus Zali <66894179+ZXL7@users.noreply.github.com>
2022-04-27 23:31:56 -0300

    Apiconsumida

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <header>
+      <img src="./assets/poke.webp" />
+      <input
+        type="text"
+        name="nome"
+        value=""
+        placeholder="Busque aqui seu pokemon"
+      />
+    </header>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+#app {
+  background: linear-gradient(to right, lightblue, darkblue);
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+}
+
+header {
+  margin: 0 auto;
+  width: 1300px;
+  padding-bottom: 50px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <header></header>
+    <h1>{{ titulo }}</h1>
+    <div class="card">
+      <ul v-for="pokemon in pokemons" :key="pokemon.name">
+        <li buscarImg>
+          <p>{{ pokemon.name }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      titulo: "PokeDex",
+      pokemons: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods: {
+    buscarImg(pokemon) {
+      return pokemon.url.split("/");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+  ul {
+    display: inline-block;
+    list-style: none;
+
+    li {
+      width: 400px;
+      height: 220px;
+      background: rgb(17, 17, 101);
+      margin-left: 20px;
+      margin-bottom: 20px;
+      text-align: center;
+      padding-top: 15px;
+
+      p {
+        font-size: 18px;
+        color: aliceblue;
+      }
+    }
+  }
+}
+</style>
